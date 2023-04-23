9640def34c0ffe3d1975ac17fae3751d37eeda1a
B4KO <69869288+B4KO@users.noreply.github.com>
2023-04-23 20:36:26 +0200

    Update

--- tcp_server.go
+++ tcp_server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"github.com/B4KO/is105sem03/mycrypt"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -37,13 +41,25 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
+
+					switch msg := string(mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)); msg {
+
 					case "ping":
+						log.Println("Dekrypter melding: ", msg)
+						msg = string(mycrypt.Krypter([]rune(msg), mycrypt.ALF_SEM03, 4))
 						_, err = c.Write([]byte("pong"))
+
 					default:
-						decryptedMessage := mycrypt.Krypter([]rune(msg), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-						log.Println("Dekrypter melding: ", string(decryptedMessage))
-						_, err = c.Write([]byte(string(decryptedMessage)))
+						log.Println("Dekrypter melding: ", msg)
+
+						if strings.HasPrefix(msg, "Kjevik;") {
+							msg = formatString(msg)
+							log.Println("Formaterer melding: ", msg)
+						}
+
+						msg = string(mycrypt.Krypter([]rune(msg), mycrypt.ALF_SEM03, 4))
+
+						_, err = c.Write([]byte(msg))
 					}
 					if err != nil {
 						if err != io.EOF {
@@ -56,4 +72,36 @@ func main() {
 		}
 	}()
 	wg.Wait()
+}
+
+func celsiusToFahrenheit(celsius int) int {
+	return int(float64(celsius)*9/5 + 32)
+}
+
+func formatString(input string) string {
+
+	r := csv.NewReader(strings.NewReader(input))
+	r.Comma = ';'
+
+	record, err := r.Read()
+	if err != nil {
+		fmt.Println("Error parsing the CSV string:", err)
+		return "Invalid format"
+	}
+
+	// Get the last value and convert it from Celsius to Fahrenheit
+	lastValue, err := strconv.Atoi(record[len(record)-1])
+	if err != nil {
+		fmt.Println("Error converting the last value to an integer:", err)
+		return "Invalid format"
+	}
+
+	fahrenheit := celsiusToFahrenheit(lastValue)
+
+	record[len(record)-1] = strconv.Itoa(fahrenheit)
+
+	// Reconstruct the CSV string with the new value
+	output := strings.Join(record, ";")
+
+	return output
 }
